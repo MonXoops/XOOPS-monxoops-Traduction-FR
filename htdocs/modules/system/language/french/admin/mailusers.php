e3ec05bf13b3206f05035bcf4fe6c766f71d3085
Grégory Mage <gregory.mage@gmail.com>
2020-02-20 21:53:10 +0100

    Traduction 2.5.10

---
+++ htdocs/modules/system/language/french/admin/mailusers.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @copyright       (c) 2000-2016 XOOPS Project (www.xoops.org)
+ * @license         GNU GPL 2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
+ * _LANGCODE    fr
+ * _CHARSET     UTF-8
+ */
+define('_AM_SYSTEM_MAILUSERS_AMIFCHECKD', 'Si cette case est cochée, tous les messages ci-dessus ainsi que la messagerie privée seront ignorés');
+define('_AM_SYSTEM_MAILUSERS_EMAIL', 'Courriel');
+define('_AM_SYSTEM_MAILUSERS_GROUPIS', ' - Groupe :');
+define('_AM_SYSTEM_MAILUSERS_DAY', ' - Nombre de jours depuis la dernière connexion :');
+define('_AM_SYSTEM_MAILUSERS_IDLEMORE', 'plus de X');
+define('_AM_SYSTEM_MAILUSERS_IDLELESS', 'moins de X');
+define('_AM_SYSTEM_MAILUSERS_DATE', ' - Date de la dernière connexion :');
+define('_AM_SYSTEM_MAILUSERS_LASTLOGMAX', 'avant');
+define('_AM_SYSTEM_MAILUSERS_LASTLOGMIN', 'après');
+define('_AM_SYSTEM_MAILUSERS_REGDATE', ' - Date d\'inscription :');
+define('_AM_SYSTEM_MAILUSERS_REGDMIN', 'après');
+define('_AM_SYSTEM_MAILUSERS_REGDMAX', 'avant');
+define('_AM_SYSTEM_MAILUSERS_INACTIVE', 'Envoyer un message aux utilisateurs inactifs uniquement');
+define('_AM_SYSTEM_MAILUSERS_MAILOK', 'Envoyer un message uniquement aux utilisateurs qui acceptent les messages de notification');
+define('_AM_SYSTEM_MAILUSERS_OPTIONAL', 'Valeur facultative');
+define('_AM_SYSTEM_MAILUSERS_LIST', 'Envoyer un message aux utilisateurs');
+define('_AM_SYSTEM_MAILUSERS_MAILBODY', 'Corps du message');
+define('_AM_SYSTEM_MAILUSERS_MAILFNAME', 'Nom de l\'expéditeur');
+define('_AM_SYSTEM_MAILUSERS_MAILFMAIL', 'Courriel de l\'expéditeur');
+define('_AM_SYSTEM_MAILUSERS_MAILTAGS', 'Mots-clés utiles :');
+define('_AM_SYSTEM_MAILUSERS_MAILTAGS1', '{X_UID} affichera l\'ID de l\'utilisateur');
+define('_AM_SYSTEM_MAILUSERS_MAILTAGS2', '{X_UNAME} affichera le nom de l\'utilisateur');
+define('_AM_SYSTEM_MAILUSERS_MAILTAGS3', '{X_UEMAIL} affichera le courriel de l\'utilisateur');
+define('_AM_SYSTEM_MAILUSERS_MAILTAGS4', '{X_UACTLINK} affichera le lien d\'activation de l\'utilisateur');
+define('_AM_SYSTEM_MAILUSERS_MAILSUBJECT', 'Sujet');
+define('_AM_SYSTEM_MAILUSERS_MANAGER', 'Gestionnaire de messages utilisateur');
+define('_AM_SYSTEM_MAILUSERS_SENDNEXT', 'Suivant');
+define('_AM_SYSTEM_MAILUSERS_NOUSERMATCH', 'Aucun utilisateur correspondant');
+define('_AM_SYSTEM_MAILUSERS_PM', 'Message privé');
+define('_AM_SYSTEM_MAILUSERS_SENDCOMP', 'Envoi du message terminé.');
+define('_AM_SYSTEM_MAILUSERS_SENDTO', 'Envoyer à');
+define('_AM_SYSTEM_MAILUSERS_SENDTOUSERS', 'Envoyer un message aux utilisateurs qui :');
+define('_AM_SYSTEM_MAILUSERS_SENDTOUSERS2', 'Envoyer à :');
+define('_AM_SYSTEM_MAILUSERS_SENT', 'Envoyé aux utilisateurs');
+define('_AM_SYSTEM_MAILUSERS_SENTNUM', '%s - %s soit un total de %s utilisateur(s)');
+define('_AM_SYSTEM_MAILUSERS_TIMEFORMAT', '(Format yyyy-mm-dd, en option)');
